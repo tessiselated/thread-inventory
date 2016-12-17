cbc1900f8945096702d857666fe103617c73eef7
Tess Elation <fredisded@gmail.com>
2016-12-17 14:55:13 +1100

    Added route handler for login page

--- app.rb
+++ app.rb
@@ -5,5 +5,17 @@ require "active_record"
 require "sinatra/activerecord"
 require "pg"
 
+require_relative "./models/user"
+
 
 set :database_file, "./config/database.yml"
+
+
+get "/" do
+  #check if active session, otherwise
+  redirect to("/login")
+end
+
+get "/login" do
+  erb :loginpage
+end
